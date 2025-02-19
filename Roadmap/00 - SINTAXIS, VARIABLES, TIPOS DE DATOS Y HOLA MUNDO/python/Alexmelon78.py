87ffdd170aeb873920d7d1996763e00f1f9acac9
Alexmelon78 <alexmelon78@gmail.com>
2025-02-19 12:39:50 -0600

    Create Alexmelon78.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Alexmelon78.py
@@ -0,0 +1,23 @@
+# https://www.python.org/
+
+# Comentario en una linea
+
+"""
+Saludos
+a
+Todos
+"""
+
+my_variable = "Esto es una variable"
+my_variable = "Es una nueva variable 2"
+
+MY_CONSTANT = "Es una constante" # por convención
+
+my_int = 1
+my_float = 1.6
+my_bool = False 
+my_bool = True
+my_string = "aqui cadena de texto"
+my_other_string = 'otra cadena de texto'
+
+print("Hola, phyton")
